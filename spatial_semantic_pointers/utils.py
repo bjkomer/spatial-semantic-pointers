a648ad8d9d32a3b6b7a0d7a5988c82b1f542cc19
Brent Komer <brent.komer@gmail.com>
2020-01-09 15:51:59 -0500

    adding functions for hexagonally defined SSPs

--- spatial_semantic_pointers/utils.py
+++ spatial_semantic_pointers/utils.py
@@ -277,3 +277,111 @@ def encode_random(x, y, dim=512, convert_to_sp=False):
         return spa.SemanticPointer(data=vec)
     else:
         return vec
+
+##################################
+# Hexagonal Coordinate Functions #
+##################################
+
+def rotate_vector(vec, rot_axis, theta):
+    axis = rot_axis.copy()
+    axis = axis/np.sqrt(np.dot(axis, axis))
+    a = np.cos(theta/2.0)
+    b, c, d = -axis*np.sin(theta/2.0)
+    aa, bb, cc, dd = a*a, b*b, c*c, d*d
+    bc, ad, ac, ab, bd, cd = b*c, a*d, a*c, a*b, b*d, c*d
+    return np.dot(np.array([[aa+bb-cc-dd, 2*(bc+ad), 2*(bd-ac)],
+                            [2*(bc-ad), aa+cc-bb-dd, 2*(cd+ab)],
+                            [2*(bd+ac), 2*(cd-ab), aa+dd-bb-cc]]),
+                  vec)
+
+
+grid_angle = 0
+
+normal = np.array([1, 1, 1])
+default_x_axis = np.array([1, -1, 0])
+default_y_axis = np.array([-1, -1, 2])
+normal = normal / np.linalg.norm(normal)
+default_x_axis = default_x_axis / np.linalg.norm(default_x_axis)
+default_y_axis = default_y_axis / np.linalg.norm(default_y_axis)
+
+default_x_axis = rotate_vector(default_x_axis, normal, grid_angle * np.pi / 180)
+default_y_axis = rotate_vector(default_y_axis, normal, grid_angle * np.pi / 180)
+
+# Used in the vectorized functions
+default_xy_axes = np.vstack([default_x_axis, default_y_axis]).T
+
+
+def xyz_to_xy(coord, x_axis=default_x_axis, y_axis=default_y_axis):
+    """
+    Projects a 3D hexagonal coordinate into the
+    corresponding 2D coordinate
+    """
+    x = np.dot(x_axis, coord)
+    y = np.dot(y_axis, coord)
+
+    return np.array([x, y])
+
+
+def xyz_to_xy_v(coords, xy_axes=default_xy_axes):
+    """
+    Projects a 3D hexagonal coordinate into the
+    corresponding 2D coordinate
+
+    coords is a (n, 3) matrix
+    xy_axes is a (3, 2) matrix
+    """
+
+    return np.dot(coords, xy_axes)
+
+
+def xy_to_xyz(coord, x_axis=default_x_axis, y_axis=default_y_axis):
+    """
+    Converts a 2D coordinate into the corresponding
+    3D coordinate in the hexagonal representation
+    """
+    return x_axis*coord[0]+y_axis*coord[1]
+
+
+def xy_to_xyz_v(coords, xy_axes=default_xy_axes):
+    """
+    Converts a 2D coordinate into the corresponding
+    3D coordinate in the hexagonal representation
+    coord is a (n, 2) matrix
+    xy_axes is a (3, 2) matrix
+    """
+
+    return np.dot(coords, xy_axes.T)
+
+
+def encode_point_hex(x, y, x_axis_sp, y_axis_sp, z_axis_sp):
+    """
+    Encodes a given 2D point as a 3D hexagonal SSP
+    """
+    xyz = xy_to_xyz((x, y))
+
+    return power(x_axis_sp, xyz[0]) * power(y_axis_sp, xyz[1]) * power(z_axis_sp, xyz[2])
+
+
+def get_heatmap_vectors_hex(xs, ys, x_axis_sp, y_axis_sp, z_axis_sp):
+    """
+    Precompute spatial semantic pointers for every location in the linspace
+    Used to quickly compute heat maps by a simple vectorized dot product (matrix multiplication)
+    """
+    if x_axis_sp.__class__.__name__ == 'SemanticPointer':
+        dim = len(x_axis_sp.v)
+    else:
+        dim = len(x_axis_sp)
+        x_axis_sp = spa.SemanticPointer(data=x_axis_sp)
+        y_axis_sp = spa.SemanticPointer(data=y_axis_sp)
+        z_axis_sp = spa.SemanticPointer(data=z_axis_sp)
+
+    vectors = np.zeros((len(xs), len(ys), dim))
+
+    for i, x in enumerate(xs):
+        for j, y in enumerate(ys):
+            p = encode_point_hex(
+                x=x, y=y, x_axis_sp=x_axis_sp, y_axis_sp=y_axis_sp, z_axis_sp=z_axis_sp
+            )
+            vectors[i, j, :] = p.v
+
+    return vectors
